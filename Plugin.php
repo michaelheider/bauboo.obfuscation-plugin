91207420b142de71fbde057fb864b4283b343175
Michael <miheider@bluewin.ch>
2020-09-09 19:36:12 +0200

    convert indentation to tab

--- Plugin.php
+++ Plugin.php
@@ -6,44 +6,44 @@ use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function registerComponents()
-    {
-        return [
-            'Bauboo\Obfuscation\Components\EMail' => 'email',
-            'Bauboo\Obfuscation\Components\Phone' => 'phone',
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function registerComponents()
+	{
+		return [
+			'Bauboo\Obfuscation\Components\EMail' => 'email',
+			'Bauboo\Obfuscation\Components\Phone' => 'phone',
+		];
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function registerSettings()
-    {
-        return [
-            'general' => [
-                'label' => 'bauboo.obfuscation::lang.plugin.name',
-                'description' => 'bauboo.obfuscation::lang.settings_item.description',
-                'icon' => 'oc-icon-at',
-                'class' => 'Bauboo\Obfuscation\Models\Settings',
-                'permissions' => ['bauboo.obfuscation.access_settings'],
-                'keywords' => 'bauboo.obfuscation::lang.settings_item.keywords',
-            ],
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function registerSettings()
+	{
+		return [
+			'general' => [
+				'label' => 'bauboo.obfuscation::lang.plugin.name',
+				'description' => 'bauboo.obfuscation::lang.settings_item.description',
+				'icon' => 'oc-icon-at',
+				'class' => 'Bauboo\Obfuscation\Models\Settings',
+				'permissions' => ['bauboo.obfuscation.access_settings'],
+				'keywords' => 'bauboo.obfuscation::lang.settings_item.keywords',
+			],
+		];
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function registerPermissions()
-    {
-        return [
-            'bauboo.obfuscation.access_settings' => [
-                'tab' => 'bauboo.obfuscation::lang.plugin.name',
-                'label' => 'bauboo.obfuscation::lang.permissions.label',
-            ],
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function registerPermissions()
+	{
+		return [
+			'bauboo.obfuscation.access_settings' => [
+				'tab' => 'bauboo.obfuscation::lang.plugin.name',
+				'label' => 'bauboo.obfuscation::lang.permissions.label',
+			],
+		];
+	}
 }
--- components/EMail.php
+++ components/EMail.php
@@ -8,107 +8,107 @@ use InvalidArgumentException;
 
 class EMail extends ComponentBase
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function componentDetails(): array
-    {
-        return [
-            'name' => 'bauboo.obfuscation::lang.components.email.name',
-            'description' => 'bauboo.obfuscation::lang.components.email.description',
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function componentDetails(): array
+	{
+		return [
+			'name' => 'bauboo.obfuscation::lang.components.email.name',
+			'description' => 'bauboo.obfuscation::lang.components.email.description',
+		];
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function defineProperties(): array
-    {
-        return [
-            'email' => [
-                'title' => 'bauboo.obfuscation::lang.components.email.email.title',
-                'description' => 'bauboo.obfuscation::lang.components.email.email.description',
-                'type' => 'string',
-                'validationPattern' => '^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}$',
-                'validationMessage' => 'bauboo.obfuscation::lang.components.email.email.validationMessage',
-                'default' => 'local@example.com',
-                'required' => true,
-            ],
-            'anchor' => [
-                'title' => 'bauboo.obfuscation::lang.components.email.anchor.title',
-                'description' => 'bauboo.obfuscation::lang.components.email.anchor.description',
-                'type' => 'string',
-                'default' => '',
-            ],
-            'subject' => [
-                'title' => 'bauboo.obfuscation::lang.components.email.subject.title',
-                'description' => 'bauboo.obfuscation::lang.components.email.subject.description',
-                'type' => 'string',
-                'default' => '',
-            ],
-            'body' => [
-                'title' => 'bauboo.obfuscation::lang.components.email.body.title',
-                'description' => 'bauboo.obfuscation::lang.components.email.body.description',
-                'type' => 'string',
-                'default' => '',
-            ],
-        ];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function defineProperties(): array
+	{
+		return [
+			'email' => [
+				'title' => 'bauboo.obfuscation::lang.components.email.email.title',
+				'description' => 'bauboo.obfuscation::lang.components.email.email.description',
+				'type' => 'string',
+				'validationPattern' => '^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}$',
+				'validationMessage' => 'bauboo.obfuscation::lang.components.email.email.validationMessage',
+				'default' => 'local@example.com',
+				'required' => true,
+			],
+			'anchor' => [
+				'title' => 'bauboo.obfuscation::lang.components.email.anchor.title',
+				'description' => 'bauboo.obfuscation::lang.components.email.anchor.description',
+				'type' => 'string',
+				'default' => '',
+			],
+			'subject' => [
+				'title' => 'bauboo.obfuscation::lang.components.email.subject.title',
+				'description' => 'bauboo.obfuscation::lang.components.email.subject.description',
+				'type' => 'string',
+				'default' => '',
+			],
+			'body' => [
+				'title' => 'bauboo.obfuscation::lang.components.email.body.title',
+				'description' => 'bauboo.obfuscation::lang.components.email.body.description',
+				'type' => 'string',
+				'default' => '',
+			],
+		];
+	}
 
-    /** @var string Local part of email. */
-    public $local;
-    /** @var string Domain part except TLD of email. */
-    public $domain;
-    /** @var string TLD part of email. */
-    public $tld;
-    /** @var bool Whether optional fields are provided. */
-    public $hasOptional;
-    /** @var string Anchor as provided by user. */
-    public $anchor;
-    /** @var string Subject as provided by user. */
-    public $subject;
-    /** @var string Body as provided by user. */
-    public $body;
+	/** @var string Local part of email. */
+	public $local;
+	/** @var string Domain part except TLD of email. */
+	public $domain;
+	/** @var string TLD part of email. */
+	public $tld;
+	/** @var bool Whether optional fields are provided. */
+	public $hasOptional;
+	/** @var string Anchor as provided by user. */
+	public $anchor;
+	/** @var string Subject as provided by user. */
+	public $subject;
+	/** @var string Body as provided by user. */
+	public $body;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function onRun(): void
-    {
-        if (Settings::get('spamspan_inject', true)) {
-            $this->addJs('assets/js/spamspan.js');
-        }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function onRun(): void
+	{
+		if (Settings::get('spamspan_inject', true)) {
+			$this->addJs('assets/js/spamspan.js');
+		}
 
-        $this->anchor = $this->property('anchor');
-        $this->subject = $this->property('subject');
-        $this->body = $this->property('body');
-        $this->hasOptional = !(($this->anchor === '') && ($this->subject === '') && ($this->body === ''));
-        $email = $this->property('email');
-        $parts = $this->parseEmail($email);
-        $this->local = $parts['local'];
-        $this->domain = $parts['domain'];
-        $this->tld = $parts['tld'];
-    }
+		$this->anchor = $this->property('anchor');
+		$this->subject = $this->property('subject');
+		$this->body = $this->property('body');
+		$this->hasOptional = !(($this->anchor === '') && ($this->subject === '') && ($this->body === ''));
+		$email = $this->property('email');
+		$parts = $this->parseEmail($email);
+		$this->local = $parts['local'];
+		$this->domain = $parts['domain'];
+		$this->tld = $parts['tld'];
+	}
 
-    /**
-     * @param string $email
-     *
-     * @return array
-     *
-     * @throws InvalidArgumentException if $email is not a valid email string
-     */
-    protected function parseEmail($email)
-    {
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Should always be OK, since we already check in the input field.
-            throw new InvalidArgumentException('E-Mail address provided is not valid.');
-        }
-        $at = strrpos($email, '@');
-        $dot = strrpos($email, '.');
-        $local = substr($email, 0, $at);
-        $domain = substr($email, $at + 1, $dot - $at - 1);
-        $tld = substr($email, $dot + 1);
+	/**
+	 * @param string $email
+	 *
+	 * @return array
+	 *
+	 * @throws InvalidArgumentException if $email is not a valid email string
+	 */
+	protected function parseEmail($email)
+	{
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			// Should always be OK, since we already check in the input field.
+			throw new InvalidArgumentException('E-Mail address provided is not valid.');
+		}
+		$at = strrpos($email, '@');
+		$dot = strrpos($email, '.');
+		$local = substr($email, 0, $at);
+		$domain = substr($email, $at + 1, $dot - $at - 1);
+		$tld = substr($email, $dot + 1);
 
-        return ['local' => $local, 'domain' => $domain, 'tld' => $tld];
-    }
+		return ['local' => $local, 'domain' => $domain, 'tld' => $tld];
+	}
 }
